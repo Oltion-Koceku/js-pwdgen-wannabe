53e33d6f2b5bc3cd12653dbf9980a053e401b0a6
Oltion-Koceku <koceku17@gmail.com>
2024-02-19 14:51:59 +0100

    [add]console

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const nome = prompt("Come ti chiami?");
+const cognome = prompt("Qual è il tuo cognome?");
+const colore = prompt("Il tuo colore preferito?");
+const datiUtente = nome + " " + cognome + " " + colore
+console.log(nome, cognome, colore, datiUtente);
