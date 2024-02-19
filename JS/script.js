29d33d7913576271f09f38c2b1e1e1145e7c5b05
Oltion-Koceku <koceku17@gmail.com>
2024-02-19 15:33:35 +0100

    fix

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 const nome = prompt("Come ti chiami?");
 const cognome = prompt("Qual è il tuo cognome?");
 const colore = prompt("Il tuo colore preferito?");
-const nomeCognome = nome + cognome;
+const nomeCognome = nome + " " + cognome;
 const coloreNumero = colore + 24;
 console.log(nome, cognome, colore, nomeCognome, coloreNumero);
 document.getElementById("password").innerHTML =
 `
 <p> 
   Ciao <strong>${nomeCognome}</strong>,<br>
-  Questa è la tua Password: <strong>${nomeCognome + coloreNumero}</strong>
+  Questa è la tua Password: <strong >${nome + cognome + coloreNumero}</strong>
 </p>
 `;
